a7bd4890468064ebbeaaf3ac1b587d1742111b04
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-04-09 14:26:03 -0700

    test passed, add price conditional

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,9 @@ Pizza.prototype.totalPrice = function() {
   if (this.toppings.length === 1) this.price += 3
   else if (this.toppings.length === 2) this.price += 6 
   else if (this.toppings.length === 3) this.price += 9 
+  if (this.size === "small") this.price += 5
+  else if (this.size === "medium") this.price += 10
+  else if (this.size === "large") this.price += 15
 }
 
 
