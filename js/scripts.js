fecb5a359afe59a718d31ca99ea54f56280b8aa0
thomasfriedrichs <friedrichs.tommy@gmail.com>
2021-04-09 12:02:56 -0700

    add failed test

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
+//Business Logic
+
 function Pizza(toppings, size) {
   this.toppings = toppings
   this.size = size 
+  this.price = 0 
 }
 
-const myPizza = new Pizza("olives", "small")
-myPizza.size;
+Pizza.prototype.totalPrice = function() {
+  this.toppings.forEach(length()) 
+    this.price += 3
+  
+}
+
+const myPizza = new Pizza(["olives, cheese, pepperoni"], "small")
+          myPizza;
+
+
+
